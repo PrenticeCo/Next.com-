509bef1d46e7f71d41ee55bf95ba9575d45b225d
Ben <benprentice1@hotmail.co.uk>
2023-12-20 23:13:00 +0000

    tidied code

--- home.js
+++ home.js
@@ -1,17 +1,22 @@
 const testApi = async () => {
-  const response = await fetch("https://6353d42fe64783fa82798680.mockapi.io/api/adobe/productItems/");
+  const response = await fetch(
+    "https://6353d42fe64783fa82798680.mockapi.io/api/adobe/productItems/"
+  );
   const items = await response.json();
   console.log(items);
 
-
   const productList = document.querySelector(".product--list");
 
-  productList.innerHTML = items.map((item) => {
-    console.log(item.colors);
-    return (
-      `
-      <a class="product--list__card" href="${item.id}.html">
-      <img class="product--list__img" src="${item.image}" alt="${item.altImage}" />
+  productList.innerHTML = items
+    .map((item) => {
+      console.log(item.colors);
+      return `
+      <a class="product--list__card" href="product.html?id=${
+        item.id
+      }" data-id="${item.id}">
+      <img class="product--list__img" src="${item.image}" alt="${
+        item.altImage
+      }" />
       <span class="card--heart__container">
         <img class="card--heart" src="/images/heart.svg" />
       </span>
@@ -19,34 +24,18 @@ const testApi = async () => {
         <h3 class="product--list__title">${item.description}</h3>
         <h3 class="product--list__price">${item.price}</h3>
         <div class="product--list__colors">
-          ${item.colors.map((color) => {
-            console.log(color); 
-            return `<div class="colors" style="background-color: ${color};"></div>`;
-          }).join('')}
+          ${item.colors
+            .map((color) => {
+              console.log(color);
+              return `<div class="colors" style="background-color: ${color};"></div>`;
+            })
+            .join("")}
         </div>
       </div>
       </a>
-      `
-    );
-  }).join('');
-  
-}
-
+      `;
+    })
+    .join("");
+};
 
 testApi();
-
-// const [cart, setShoppingCart] = useState([]);
-
-// const addToCart = (id) => {
-
-// console.log("id:", id)
-// setShoppingCart(
-//   [
-//     ...shoppingCart,
-//     {...obj}
-//   ]
-//   );
-// console.log(shoppingCart)
-// }
-
-// const {id, itemName, price, itemImage} = props.data;
--- product.js
+++ product.js
@@ -1,34 +1,41 @@
+const searchParams = new URLSearchParams(window.location.search);
+const id = searchParams.get("id");
+console.log(id);
+
+const productItemColor = document.querySelector(
+  ".product--item--dropdowns__color"
+);
+const colorDiv = document.querySelector(
+  ".product--item--dropdowns__color__icon"
+);
+const productDescription = document.querySelector(".product--item__title");
+const productPrice = document.querySelector(".product--item__price");
+const image = document.querySelector(".image--holder");
+const colorDropdown = document.querySelector(
+  ".product--item--dropdowns__color"
+);
+
 const testApi = async () => {
-    const response = await fetch("https://6353d42fe64783fa82798680.mockapi.io/api/adobe/productItems/");
-    const items = await response.json();
-    console.log(items);
+  const response = await fetch(
+    `https://6353d42fe64783fa82798680.mockapi.io/api/adobe/productItems/${id}`
+  );
+  const items = await response.json();
+  console.log(items);
 
-    const productItemColor = document.querySelector(".product--item--dropdowns__color");
-    const firstItem = items[0];
-    const colors = firstItem.colors;
+  const colorsHTML = items.colors
+    .map((item) => `<option value="${item}">${item}</option>`)
+    .join("");
+  console.log(colorsHTML);
+  colorDropdown.insertAdjacentHTML("beforeend", colorsHTML);
 
-    colors.forEach(color => {
-        const colors = firstItem.colors;  
-                const colorOption = document.createElement("option");
-                colorOption.value = color;
-                colorOption.text = color;
-                productItemColor.appendChild(colorOption);
-        
-    });
+  productItemColor.addEventListener("change", function () {
+    const selectedColor = productItemColor.value;
+    colorDiv.style.backgroundColor = selectedColor;
+  });
 
-   
-    const colorDiv = document.querySelector(".product--item--dropdowns__color__icon"); 
-    const initialColor = productItemColor.value;
-        colorDiv.style.backgroundColor = initialColor;
-
-    productItemColor.addEventListener("change", function() {
-        const selectedColor = productItemColor.value;
-        colorDiv.style.backgroundColor = selectedColor;
-        
-    });
-}
+  productDescription.innerHTML = items.description;
+  productPrice.innerHTML = items.price;
+  image.innerHTML = `<img src="${items.image}" alt="${items.imageAlt}"/>`;
+};
 
 testApi();
-
-
-
