46959d31707902d9a654f1278e1c26347c259b0e
Ben <benprentice1@hotmail.co.uk>
2023-12-20 12:53:47 +0000

    completed js for color selection

--- product.js
+++ product.js
@@ -4,30 +4,28 @@ const testApi = async () => {
     console.log(items);
 
     const productItemColor = document.querySelector(".product--item--dropdowns__color");
+    const firstItem = items[0];
+    const colors = firstItem.colors;
 
-    items.forEach(item => {
-        const colors = item.colors;
-
-        
-            colors.forEach(color => {
+    colors.forEach(color => {
+        const colors = firstItem.colors;  
                 const colorOption = document.createElement("option");
                 colorOption.value = color;
                 colorOption.text = color;
                 productItemColor.appendChild(colorOption);
-            });
         
     });
 
+   
     const colorDiv = document.querySelector(".product--item--dropdowns__color__icon"); 
+    const initialColor = productItemColor.value;
+        colorDiv.style.backgroundColor = initialColor;
 
     productItemColor.addEventListener("change", function() {
         const selectedColor = productItemColor.value;
         colorDiv.style.backgroundColor = selectedColor;
         
     });
-
-    const initialColor = productItemColor.value;
-        colorDiv.style.backgroundColor = initialColor;
 }
 
 testApi();
