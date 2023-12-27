bdb3497165412e0a149f1306af034545671756b6
Ben <benprentice1@hotmail.co.uk>
2023-12-27 13:53:28 +0000

    temporarily removed DOMContentLoaded func

--- cart.js
+++ cart.js
@@ -1,41 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const addToCartButton = document.querySelector(".bag");
-  const cartContent = document.querySelector(".cart--items");
+// document.addEventListener("DOMContentLoaded", function () {
+const addToCartButton = document.querySelector(".bag");
+const cartContent = document.querySelector(".cart--items");
 
-  addToCartButton.addEventListener("click", function () {
-    const selectedColor = productItemColor.value;
-    const selectedTitle = productDescription.innerHTML;
-    const selectedPrice = productPrice.innerHTML;
+addToCartButton.addEventListener("click", function () {
+  const selectedColor = productItemColor.value;
+  const selectedTitle = productDescription.innerHTML;
+  const selectedPrice = productPrice.innerHTML;
 
-    const cartItem = {
-      color: selectedColor,
-      title: selectedTitle,
-      price: selectedPrice,
-    };
+  console.log("clicked");
 
-    const existingCartItems = JSON.parse(localStorage.getItem("cart")) || [];
+  const cartItem = {
+    color: selectedColor,
+    title: selectedTitle,
+    price: selectedPrice,
+  };
 
-    existingCartItems.push(cartItem);
+  const existingCartItems = JSON.parse(localStorage.getItem("cart")) || [];
 
-    localStorage.setItem("cart", JSON.stringify(existingCartItems));
+  existingCartItems.push(cartItem);
 
-    alert("Added to cart");
-  });
+  localStorage.setItem("cart", JSON.stringify(existingCartItems));
 
-  const cartFunction = () => {
-    const existingCartItems = JSON.parse(localStorage.getItem("cart")) || [];
-    cartContent.innerHTML = existingCartItems
-      .map(
-        (item) => `
-          
+  alert("Added to cart");
+});
+
+const cartFunction = () => {
+  const existingCartItems = JSON.parse(localStorage.getItem("cart")) || [];
+  cartContent.innerHTML = existingCartItems
+    .map(
+      (item) => `
             <div class="cart--items__title">${item.title}</div>
             <div class="cart--items__price">${item.price}</div>
             <div class="cart--items__color">${item.color}</div>
-          
         `
-      )
-      .join("");
-  };
+    )
+    .join("");
+};
 
-  cartFunction();
-});
+cartFunction();
+// });
