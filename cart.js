b73c19d13178f0844034a5afd8397ddfef267c60
Ben <benprentice1@hotmail.co.uk>
2024-01-10 14:07:42 +0000

    Updated cart js and css

--- cart.js
+++ cart.js
@@ -1,6 +1,8 @@
 // document.addEventListener("DOMContentLoaded", function () {
 
 const cartContent = document.querySelector(".cart--items");
+const cartPriceTotal = document.querySelector(".cart--items__price");
+const removeButton = document.querySelectorAll(".remove--btn");
 
 const cartFunction = () => {
   const existingCartItems = JSON.parse(localStorage.getItem("cart")) || [];
@@ -10,38 +12,81 @@ const cartFunction = () => {
   cartContent.innerHTML = existingCartItems
     .map(
       (item) => `
-            
-            <div class="cart--items__content data-id="${item.id}"">
-            <hr>
-            <div class="cart--items__id">${item.id}</div>
-            <div class="cart--items__title">${item.title}</div>
-            <div class="cart--items__price">Price: ${item.price}</div>
-            <div class="cart--items__color">Color: ${item.color}</div>
-            <div class="cart--items__size">Size: ${item.size}</div>
-            Quantity: 
-            <select class="cart--items__quantity">
-            <option value="${item.quantity}" selected>${item.quantity}</option>
-            </select>
-            <button class="remove--btn" type="button" name="remove--btn">REMOVE</button>
-            <hr>
-            </div>
-            
-        `
+      <div class="cart--items__content data-id="${item.id}">
+        <hr>
+        <div class="cart--items__id">${item.id}</div>
+        <div class="cart--items__title">${item.title}</div>
+        <div class="cart--items__color">Color: ${item.color}</div>
+        <div class="cart--items__size">Size: ${item.size}</div>
+        <div class="cart--items__quantity">Quantity: ${item.quantity}</div>
+        <div class="cart--items__price">Price: £${
+          parseFloat(item.price.replace("£", "")) * item.quantity
+        }</div>
+        <button class="remove--btn" type="button" name="remove--btn">REMOVE</button>
+        <hr>
+      </div>
+      `
     )
     .join("");
 };
 
 cartFunction();
 
-const removeButton = document.querySelectorAll(".remove--btn");
-console.log(removeButton);
+const cartColor = document.querySelector(".cart--items__color");
+const cartSize = document.querySelector(".cart--items__size");
+const cartPrice = document.querySelector(".cart--items__price");
+const cartId = document.querySelector(".cart--items__id");
+const cartTitle = document.querySelector(".cart--items__title");
+const cartQuantity = document.querySelector(".cart--items__quantity");
 
-for (let i = 0; i < removeButton.length; i++) {
-  let button = removeButton[i];
-  button.addEventListener("click", function (event) {
-    let buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
-  });
-}
+const addToCart = (event) => {
+  event.preventDefault();
+  const localStorageItems = localStorage.getItem("cart");
 
-// });
+  const newItem = {
+    color: cartColor.value,
+    title: cartTitle.innerHTML,
+    price: cartPrice.innerHTML,
+    quantity: cartQuantity.value,
+    size: cartSize.value,
+    id: cartId.innerHTML,
+  };
+
+  if (!localStorageItems) {
+    localStorage.setItem("cart", JSON.stringify([newItem]));
+    cartFunction();
+    return;
+  }
+
+  const itemsArr = JSON.parse(localStorageItems);
+
+  const existingItem = itemsArr.find(
+    (itemInCart) =>
+      itemInCart.id === cartId.innerHTML &&
+      itemInCart.color === cartColor.value &&
+      itemInCart.size === cartSize.value
+  );
+
+  if (existingItem) {
+    const updateItem = itemsArr.map((itemInCart) =>
+      itemInCart.id === cartId.innerHTML &&
+      itemInCart.color === cartColor.value &&
+      itemInCart.size === cartSize.value
+        ? {
+            ...itemInCart,
+            quantity: Number(itemInCart.quantity) + Number(quantity.value),
+          }
+        : itemInCart
+    );
+    localStorage.setItem("cart", JSON.stringify(updateItem));
+    cartFunction();
+    return;
+  }
+
+  localStorage.setItem("cart", JSON.stringify([...itemsArr, newItem]));
+  cartFunction();
+};
+
+addToCart();
+
+addToCartButton.addEventListener("click", addToCart);
